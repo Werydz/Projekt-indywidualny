67b54113d283e7f2badbc49900e26555fbd8fde5
Werydz <115559722+Werydz@users.noreply.github.com>
2023-12-25 18:04:04 +0100

    nawigacja i splashscreen

--- app/src/main/java/com/example/kolko_i_krzyzyk/MainActivity.kt
+++ app/src/main/java/com/example/kolko_i_krzyzyk/MainActivity.kt
@@ -3,14 +3,36 @@ package com.example.kolko_i_krzyzyk
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.kolko_i_krzyzyk.ui.theme.Kolko_i_krzyzykTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +45,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    Nawigacja()
                 }
             }
         }
@@ -35,6 +58,76 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun Nawigacja(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "splash_screen" ){
+        composable("splash_screen"){
+            SplashScreen(navController)
+        }
+        composable("main_screen"){
+            MainScreen(navController)
+        }
+        composable("tic_tac_toe"){
+            TicTacToe(navController)
+        }
+        composable("square"){
+            Square(navController)
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavHostController) {
+    LaunchedEffect(key1 = true){
+        delay(3000L)
+        navController.navigate("main_screen")
+    }
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Box(Modifier.height(200.dp)){
+            Image(painter = painterResource(id = R.drawable.icon), contentDescription = "Logo", Modifier.size(195.dp))
+        }
+        Spacer(Modifier.height(65.dp))
+        Text(text = "Autor: Weronika Rydz", fontSize = 25.sp, color = Color.Black)
+        Text(text = "Temat: Kółko i krzyżyk", fontSize = 25.sp, color = Color.Black)
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavHostController) {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Button(onClick = { /*TODO*/ }) {
+            
+        }
+        Spacer(Modifier.height(65.dp))
+        Button(onClick = { /*TODO*/ }) {
+            
+        }
+    }
+}
+
+@Composable
+fun TicTacToe(navController: NavHostController) {
+
+}
+
+@Composable
+fun Square(navController: NavHostController) {
+
 }
 
 @Preview(showBackground = true)
