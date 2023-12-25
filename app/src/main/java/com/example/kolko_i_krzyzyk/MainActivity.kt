9fd49b77c1d968914aedc5c2de0e81bafb822a2c
Werydz <115559722+Werydz@users.noreply.github.com>
2023-12-25 13:04:43 +0100

    strat

---
+++ app/src/main/java/com/example/kolko_i_krzyzyk/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.kolko_i_krzyzyk
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kolko_i_krzyzyk.ui.theme.Kolko_i_krzyzykTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Kolko_i_krzyzykTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Kolko_i_krzyzykTheme {
+        Greeting("Android")
+    }
+}
